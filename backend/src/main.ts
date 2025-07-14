eda4bb7af693eaf272f5b1b86657627d2b249874
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-07-13 22:40:32 -0300

    feat(backend): ajuste na porta

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     // Esta é a linha CRÍTICA que você precisa verificar:
-    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+    await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
     console.log(`Aplicação está rodando em: ${await app.getUrl()}`); // Mantenha esta linha para depuração!
 }
 bootstrap();
